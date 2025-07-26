e80bb0c9b6510377d4bfead10e9c85e6a9023b6e
aminergb <darkyalb144@gmail.com>
2025-07-26 10:18:31 +0200

    setup react typescript project 1

--- eslint.config.js
+++ eslint.config.js
@@ -10,48 +10,70 @@ const formattingRules = {
   'comma-dangle': ['error', 'never'],
   'space-before-function-paren': ['error', 'never'],
   'object-curly-spacing': ['error', 'always']
+  ,
+  'linebreak-style': ['error', 'unix'] // 👈 ajoute cette ligne
+  ,
+  // 👇 Line spacing rules
+  'padding-line-between-statements': [
+    'error',
+    { blankLine: 'always', prev: 'import', next: '*' },
+    { blankLine: 'any', prev: 'import', next: 'import' },
+    { blankLine: 'always', prev: '*', next: 'return' },
+    { blankLine: 'always', prev: 'block-like', next: '*' }
+  ],
+  'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }]
+
 }
 
-
-export default [
-  // ✅ JavaScript files
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module'
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...formattingRules
-    }
+const jsConfig = {
+  files: ['**/*.js'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
   },
-
-  // ✅ TypeScript + React files
+  rules: {
+    ...js.configs.recommended.rules,
+    ...formattingRules
+  }
+}
+//sets parser ! 
+const tsConfig = tseslint.config([
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        project: ['./tsconfig.app.json', './tsconfig.node.json']
-      }
-    },
+    ignores: ['dist', '**/*.min.js', 'node_modules']
+
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
       react: reactPlugin,
       'react-hooks': reactHooksPlugin
     },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname
+      }
+    },
     rules: {
-      ...tseslint.configs.recommended.rules,
       ...formattingRules,
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react/jsx-uses-react': 'warn',
       'react/jsx-uses-vars': 'warn'
-
     }
+    ,
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+
   }
+])
+
+export default [
+  jsConfig,
+  ...tsConfig
 ]
